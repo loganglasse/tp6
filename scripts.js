80ed6747b11dbe4d41856fdd22416c9996aae174
loganglasse <logan.glasse@wsu,edu>
2022-10-10 05:53:38 +0000

    updated w ajax

--- scripts.js
+++ scripts.js
@@ -1,7 +1,40 @@
-//javascript for tp5
+//javascript for tp6
+
+// function to load a file from url "fromFile" into object identified by "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML += this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
+
+
 
 
 window.onload = function() {
+  
+  
+  loadFileInto("ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol");
+  
   
   document.querySelector("#titleBanner h1").classList.add("tp");
   
@@ -21,6 +54,8 @@ window.onload = function() {
     this.classList.toggle("tpShow");
   }
   
-  document.querySelector("#recipeColumns #ingredients ul").innerHTML += "<li>a hunger for bread</li>";
+  setTimeout(function () {
+    document.querySelector("#recipeColumns #ingredients ul").innerHTML += "<li>a hunger for bread</li>";
+  }, 100);
   
 } //end window.onload
